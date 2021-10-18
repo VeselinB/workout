286e003aa2d3fd8b2f687498528fc9591132955d
VeselinB <veselin.budinov@gmail.com>
2021-10-18 11:24:32 +0300

    finished

--- src/app/workouts/workouts.component.spec.ts
+++ src/app/workouts/workouts.component.spec.ts
@@ -1,6 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { reducer } from './workout.reducer';
+import { StoreModule } from '@ngrx/store';
 import { WorkoutsComponent } from './workouts.component';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { MaterialModule } from '../material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ExersiceClass } from '../models/exersice';
+
+export class MatDialogMock {
+
+  open() {
+    return {
+      afterClosed: () => of({ action: true })
+    };
+  }
+}
 
 describe('WorkoutsComponent', () => {
   let component: WorkoutsComponent;
@@ -8,9 +24,17 @@ describe('WorkoutsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WorkoutsComponent ]
+      imports: [
+        MaterialModule, BrowserAnimationsModule,
+        HttpClientModule,
+        StoreModule.forRoot({
+          workout: reducer,
+        })
+      ],
+      declarations: [WorkoutsComponent],
+      providers: [{ provide: MatDialog, useClass: MatDialogMock }]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -22,4 +46,25 @@ describe('WorkoutsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("should create new exercise", () => {
+    let exercise = new ExersiceClass("id", "Title");
+    expect(typeof exercise).toBe('object');
+    expect(exercise.title).toBe("Title");
+    expect(exercise.id).toBe("id");
+    expect(exercise.status).toBe(false);
+  });
+
+  it('should display a new empty workout', () => {
+    component.activeWorkOuts = [{
+      id: "1",
+      exersices: {},
+      status: false,
+      title: "Test",
+    }];
+    fixture.detectChanges();
+   expect(fixture).toMatchSnapshot();
+  });
 });
+
+
